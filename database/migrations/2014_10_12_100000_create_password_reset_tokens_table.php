0e47e92ca8452847a4110ee92c65027ea2cc5c4f
unknown <barasabrandon>
2023-07-19 22:58:29 +0300

    m

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+        
+            $table->primary('email');
         });
     }
 
