9d33da28fa07d5ced2068e6931e78ad2024d44ef
unknown <barasabrandon>
2023-07-20 07:20:46 +0300

    m

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,34 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\MigrationsStarted;
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+        /**
+         * Register any application services.
+         *
+         * @return void
+         */
+        public function register() {
+    
+            // check this one here https://github.com/laravel/framework/issues/33238#issuecomment-897063577
+            Event::listen(MigrationsStarted::class, function (){
+                if (config('databases.allow_disabled_pk')) {
+                    DB::statement('SET SESSION sql_require_primary_key=0');
+                }
+            });
+    
+            Event::listen(MigrationsEnded::class, function (){
+                if (config('databases.allow_disabled_pk')) {
+                    DB::statement('SET SESSION sql_require_primary_key=1');
+                }
+            });
+        } 
 
     /**
      * Bootstrap any application services.
